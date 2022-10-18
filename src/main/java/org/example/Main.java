c2857b5bbb5aa9cc78d900b315e65438da059630
Yaroslav Turchyn <tur.chynip@gmail.com>
2022-10-18 22:21:30 +0300

    fix try syntax issue

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,7 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        try(var connection= DriverManager.getConnection(JDBCConstans.URL,JDBCConstans.USERNAME,JDBCConstans.PASSWORD);
-        {
+        try (var connection = DriverManager.getConnection(JDBCConstans.URL, JDBCConstans.USERNAME, JDBCConstans.PASSWORD)) {
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
