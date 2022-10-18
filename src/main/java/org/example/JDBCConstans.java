5cdb0adb8c82f01f221837499c3614677b377a47
Yaroslav Turchyn <tur.chynip@gmail.com>
2022-10-18 22:13:24 +0300

    initial sources

---
+++ src/main/java/org/example/JDBCConstans.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class JDBCConstans {
+    public static final String URL= "jdbc:mysql://localhost/cursor_jdbc";
+    public static final String USERNAME="root";
+    public static final String PASSWORD="turchynip";
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        try(var connection= DriverManager.getConnection(JDBCConstans.URL,JDBCConstans.USERNAME,JDBCConstans.PASSWORD);
+        {
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
