09a9ae8299d0396f666275edec81b1db7176e53a
Yaroslav Turchyn <tur.chynip@gmail.com>
2022-10-19 13:47:19 +0300

    add SQLQueries

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,67 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+
 
 public class Main {
     public static void main(String[] args) {
         try (Connection connection = DriverManager.getConnection(JDBCConstans.URL, JDBCConstans.USERNAME, JDBCConstans.PASSWORD)) {
             PreparedStatement preparedStatement = connection.prepareStatement(SQLQueries.CREATE_TABLE);
-//            preparedStatement.execute(SQLQueries.CREATE_TABLE);
-//            preparedStatement.execute(SQLQueries.INSERT_USERS);
-//            PreparedStatement preparedStatement1 = connection.prepareStatement(SQLQueries.UPDATE_AGE);
-//            preparedStatement1.setInt(1,333);
-//            preparedStatement1.setInt(2,2);
+            preparedStatement.execute();
 
-            PreparedStatement preparedStatement2 = connection.prepareStatement(SQLQueries.UPDATE_NAME);
-            preparedStatement2.setString(1, "Tetjana");
-            preparedStatement2.setInt(2, 2);
-            preparedStatement2.executeUpdate();
+//            PreparedStatement insertUsers=connection.prepareStatement(SQLQueries.INSERT_USERS);
+//            insertUsers.execute();
+
+
+            ResultSet resultSetYounger18 = preparedStatement.executeQuery(SQLQueries.YOUNGER18);
+            ArrayList<User> yonger18 = new ArrayList<>();
+            while (resultSetYounger18.next()) {
+                User user = new User();
+                user.setId(resultSetYounger18.getInt("id"));
+                user.setFirst_name(resultSetYounger18.getString("first_name"));
+                user.setLast_name(resultSetYounger18.getString("last_name"));
+                user.setAge(resultSetYounger18.getInt("age"));
+
+                yonger18.add(user);
+
+            }
+            yonger18.forEach(System.out::println);
+            System.out.println("________________________");
+            ResultSet resultSetNamesendsO = preparedStatement.executeQuery(SQLQueries.NAME_ENDS_O);
+            ArrayList<User> namesEndsO = new ArrayList<>();
+            while (resultSetNamesendsO.next()) {
+                User user = new User();
+                user.setId(resultSetNamesendsO.getInt("id"));
+                user.setFirst_name(resultSetNamesendsO.getString("first_name"));
+                user.setLast_name(resultSetNamesendsO.getString("last_name"));
+                user.setAge(resultSetNamesendsO.getInt("age"));
+
+                namesEndsO.add(user);
+
+
+            }
+            namesEndsO.forEach(System.out::println);
+
+            System.out.println("________________________");
+            ResultSet resultSetAgeBetween = preparedStatement.executeQuery(SQLQueries.AGE_BETWEEN);
+            ArrayList<User> ageBetween = new ArrayList<>();
+            while (resultSetAgeBetween.next()) {
+                User user = new User();
+                user.setId(resultSetAgeBetween.getInt("id"));
+                user.setFirst_name(resultSetAgeBetween.getString("first_name"));
+                user.setLast_name(resultSetAgeBetween.getString("last_name"));
+                user.setAge(resultSetAgeBetween.getInt("age"));
+                ageBetween.add(user);
+
+
+            }
+            ageBetween.forEach(System.out::println);
+
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        }
     }
 
-}
--- src/main/java/org/example/SQLQueries.java
+++ src/main/java/org/example/SQLQueries.java
@@ -10,10 +10,14 @@ public class SQLQueries {
             """;
 
     public final static String INSERT_USERS = """
-            INSERT INTO user (first_name, last_name,age) VALUES ('Petro', 'Ravlyk', 25),
+            INSERT INTO user (first_name, last_name,age) VALUES 
+            ('Petro', 'Ravlyk', 25),
             ('Anna','Kolombo',33),
-            ('Olha', 'Trend',45)
-            """;
+            ('Ivan', 'Koma',21),
+            ('Yaroslav', 'Pauk',13),
+            ('Oleksandr', 'Tarakan',44),
+            ('Olha', 'Trach',65) 
+             """;
 
     public final static String UPDATE_AGE = """
             UPDATE user SET age=? WHERE id=?
@@ -21,5 +25,16 @@ public class SQLQueries {
 
     public final static String UPDATE_NAME = """
             Update user Set first_name=? WHERE id=?
+            """;
+
+    public static final String YOUNGER18 = """
+            SELECT * FROM user WHERE age<35
+            """;
+    public static final String NAME_ENDS_O = """
+            SELECT * FROM user WHERE first_name LIKE '%0'
+            """;
+
+    public static final String AGE_BETWEEN = """
+            SELECT * FROM user WHERE age BETWEEN 18 and 60
             """;
 }
